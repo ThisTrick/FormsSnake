fe19da48a193b9a3717c09e744de3f356e74a4a9
ThisTrick <way.trickster@gmail.com>
2019-10-19 17:03:49 +0300

    Добавьте файлы проекта.

---
+++ FormsSnake/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FormsSnake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Snake0 = new System.Windows.Forms.Panel();
+            this.Snake1 = new System.Windows.Forms.Panel();
+            this.Snake2 = new System.Windows.Forms.Panel();
+            this.SnakeGo = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Snake0
+            // 
+            this.Snake0.BackColor = System.Drawing.SystemColors.Info;
+            this.Snake0.Location = new System.Drawing.Point(0, 0);
+            this.Snake0.Name = "Snake0";
+            this.Snake0.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Snake0.Size = new System.Drawing.Size(32, 32);
+            this.Snake0.TabIndex = 0;
+            // 
+            // Snake1
+            // 
+            this.Snake1.BackColor = System.Drawing.SystemColors.Info;
+            this.Snake1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Snake1.Location = new System.Drawing.Point(32, 0);
+            this.Snake1.Name = "Snake1";
+            this.Snake1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Snake1.Size = new System.Drawing.Size(32, 32);
+            this.Snake1.TabIndex = 1;
+            // 
+            // Snake2
+            // 
+            this.Snake2.BackColor = System.Drawing.SystemColors.Info;
+            this.Snake2.Location = new System.Drawing.Point(64, 0);
+            this.Snake2.Name = "Snake2";
+            this.Snake2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Snake2.Size = new System.Drawing.Size(32, 32);
+            this.Snake2.TabIndex = 2;
+            // 
+            // SnakeGo
+            // 
+            this.SnakeGo.Enabled = true;
+            this.SnakeGo.Interval = 200;
+            this.SnakeGo.Tick += new System.EventHandler(this.SnakeGo_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 748);
+            this.Controls.Add(this.Snake2);
+            this.Controls.Add(this.Snake1);
+            this.Controls.Add(this.Snake0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(800, 748);
+            this.MinimumSize = new System.Drawing.Size(800, 748);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Snake0;
+        private System.Windows.Forms.Panel Snake1;
+        private System.Windows.Forms.Panel Snake2;
+        private System.Windows.Forms.Timer SnakeGo;
+    }
+}
+
---
+++ FormsSnake/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsSnake
+{
+    public partial class Form1 : Form
+    {
+        public const int Space = 576;
+        public Point directionSnake  = new Point(32, 0);
+        public Point pointSnake0;
+        public Point pointSnake1;
+        public Point pointSnake2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pointSnake0 = new Point(96, 64);
+            pointSnake1 = new Point(64, 64);
+            pointSnake2 = new Point(32, 64);
+            Snake0.Location = pointSnake0;
+            Snake1.Location = pointSnake1;
+            Snake2.Location = pointSnake2;
+
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.W)
+            {
+                if(directionSnake != new Point(0, 32))
+                {
+                    directionSnake = new Point(0, -32);
+                }  
+            }
+            else if (e.KeyData == Keys.S)
+            {
+                if (directionSnake != new Point(0, -32))
+                {
+                    directionSnake = new Point(0, 32);
+                }
+            }
+            else if (e.KeyData == Keys.A)
+            {
+                if (directionSnake != new Point(32, 0))
+                {
+                    directionSnake = new Point(-32, 0);
+                }
+                
+            }
+            else if (e.KeyData == Keys.D)
+            {
+                if (directionSnake != new Point(-32, 0))
+                {
+                    directionSnake = new Point(32, 0);
+                }
+            }
+            else if (e.KeyData == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void SnakeGo_Tick(object sender, EventArgs e)
+        {
+            if (Snake0.Location.X <= Space && Snake0.Location.Y <= Space && Snake0.Location.X >= 0 && Snake0.Location.Y >= 0)
+            {
+                pointSnake2 = Snake1.Location;
+                pointSnake1 = Snake0.Location;
+                pointSnake0.X += directionSnake.X;
+                pointSnake0.Y += directionSnake.Y;
+                Snake0.Location = pointSnake0;
+                Snake1.Location = pointSnake1;
+                Snake2.Location = pointSnake2;
+            }
+            else
+            {
+                if (Snake0.Location.X > Space)
+                {
+                    if(Snake0.Location.Y == Space)
+                    {
+                        pointSnake0.Y -= 32;
+                    }
+                    else
+                    {
+                        pointSnake0.Y += 32;
+                    }
+                    pointSnake0.X -= 32;
+                    directionSnake = new Point(-32, 0);
+                }
+                else if (Snake0.Location.X < 0)
+                {
+                    if (Snake0.Location.Y == Space)
+                    {
+                        pointSnake0.Y -= 32;
+                    }
+                    else
+                    {
+                        pointSnake0.Y += 32;
+                    }
+                    
+                    pointSnake0.X += 32;
+                    directionSnake = new Point(32, 0);
+                }
+                else if (Snake0.Location.Y > Space)
+                {
+                    if (Snake0.Location.X == Space)
+                    {
+                        pointSnake0.X -= 32;
+                    }
+                    else
+                    {
+                        pointSnake0.X += 32;
+                    }
+                    pointSnake0.Y -= 32;
+                    directionSnake = new Point(0, -32);
+                }
+                else if (Snake0.Location.Y < 0)
+                {
+                    if (Snake0.Location.X == Space)
+                    {
+                        pointSnake0.X -= 32;
+                    }
+                    else
+                    {
+                        pointSnake0.X += 32;
+                    }
+                    pointSnake0.Y += 32;
+                    directionSnake = new Point(0, 32);
+                }
+                else
+                {
+                    MessageBox.Show("Ты дебил");
+                }
+
+                Snake0.Location = pointSnake0;
+                Snake1.Location = pointSnake1;
+                Snake2.Location = pointSnake2;
+            }
+        }
+    }
+}
