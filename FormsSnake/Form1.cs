583c97850e60713e06b0232eff8547b90a58aaf4
ThisTrick <way.trickster@gmail.com>
2019-10-20 19:44:04 +0300

    Добавлено перетаскивание окна.

--- FormsSnake/Form1.cs
+++ FormsSnake/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,6 +21,12 @@ namespace FormsSnake
 
         public List<Panel> snake = new List<Panel>();
         public List<Point> pointsSnake = new List<Point>();
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+        extern static void ReleaseCapture();
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
 
 
         public Form1()
@@ -198,6 +205,12 @@ namespace FormsSnake
                     snake[i].Location = pointsSnake[i];
                 }
             }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {           
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
