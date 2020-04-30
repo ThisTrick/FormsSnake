a8b134a6c2dfbd4ed6dac5f1a42c8a633c090509
ThisTrick <way.trickster@gmail.com>
2020-04-30 22:40:00 +0300

    Испавляю не гибкость решения. Создан метод CreateSnakeItem и Create Panel

--- FormsSnake/Form1.cs
+++ FormsSnake/Form1.cs
@@ -41,37 +41,33 @@ namespace FormsSnake
         {
             InitializeComponent();
             space = 576;
-            directionSnake = new Point(32, 0);
+
+            ////То что нужно будет удалить 
             hide = new Point(600, 600);
-            lengthSnake = 2;
-            snake = new List<Panel>();
             pointsSnake = new List<Point>(){
                 new Point(96, 64),
                 new Point(64, 64),
                 new Point(32, 64)
                 };
-            rand = new Random(Guid.NewGuid().GetHashCode());
+            ////
 
             directionLeft = new Point(32, 0);
             directionRight = new Point(-32, 0);
             directionTop = new Point(0, -32);
             directionBottom = new Point(0, 32);
+            directionSnake = directionLeft;
 
+            lengthSnake = 2;
+            snake = new List<Panel>() {
+                CreateSnakeItem(new Point(96, 64)),
+                CreateSnakeItem(new Point(64, 64)),
+                CreateSnakeItem(new Point(32, 64)),
+            };
 
-            IEnumerable<Control> controls = Controls.Cast<Control>();
-            snake.AddRange(controls.Where(x => x.Name.Contains("snake") && x is Panel).ToArray().Cast<Panel>().Reverse());
+            rand = new Random(Guid.NewGuid().GetHashCode());
 
-            for (int i = 2; i <= 50; i++)
-            {
-                pointsSnake.Add(hide);
-            }
-
-            for (int i = 0; i <= 50; i++)
-            {
-                snake[i].Location = pointsSnake[i];
-            }
-
-            label1.Text = (lengthSnake - 2).ToString();
+            snake0.Visible = true;
+            Count.Text = (lengthSnake - 2).ToString();
             Apple.Location = new Point(96, 96);
 
         }
@@ -117,25 +113,56 @@ namespace FormsSnake
             }
         }
 
+
         private void SnakeGo_Tick(object sender, EventArgs e)
         {
             
             if (snake0.Location.X <= space && snake0.Location.Y <= space && snake0.Location.X >= 0 && snake0.Location.Y >= 0)
             {
                 SnakeMoving();
+                /*
                 SnakeDeath();
 
                 if (snake[0].Location == Apple.Location)
                 {
                     EatAnApple();
                 }
+                */
             }
             else
             {
+                /*
                 SnakeHide();
                 SnakeStartingPoint();
+                */
             }
         }
+        /// <summary>
+        /// Создает панель для части змейки. 
+        /// </summary>
+        /// <param name="location">Локация объекта Panel</param>
+        /// <returns></returns>
+        private Panel CreateSnakeItem(Point location)
+        {
+            var snakeItem = CreatePanel(location);
+            snakeItem.BackColor = SystemColors.Info;
+            
+            return snakeItem;
+        }
+        /// <summary>
+        /// Создает панель по шаблону.
+        /// </summary>
+        /// <param name="location">Локация объекта Panel</param>
+        /// <returns></returns>
+        private Panel CreatePanel(Point location)
+        {
+            var panel = new Panel();
+            panel.Size = new Size(26, 26);
+            panel.Location = location;
+            Controls.Add(panel);
+            return panel;
+        }
+
         /// <summary>
         /// Удар змейки об себя.
         /// Теряет все блоки кроме первых 3.
@@ -162,7 +189,7 @@ namespace FormsSnake
                 pointsSnake[j] = hide;
             }
             lengthSnake = 2;
-            label1.Text = (lengthSnake - 2).ToString();
+            Count.Text = (lengthSnake - 2).ToString();
         }
 
         /// <summary>
@@ -172,11 +199,10 @@ namespace FormsSnake
         {
             for (int i = lengthSnake; i > 0; i--)
             {
-                pointsSnake[i] = snake[i - 1].Location;
-                snake[i].Location = pointsSnake[i];
+                var tmp = snake[i - 1].Location;
+                snake[i].Location = tmp;
             }
-            pointsSnake[0] = new Point(pointsSnake[0].X + directionSnake.X, pointsSnake[0].Y + directionSnake.Y);
-            snake[0].Location = pointsSnake[0];
+            snake[0].Location = new Point(snake[0].Location.X + directionSnake.X, snake[0].Location.Y + directionSnake.Y);
         }
 
         /// <summary>
@@ -189,7 +215,7 @@ namespace FormsSnake
             if (lengthSnake < snake.Count - 1)
             {
                 lengthSnake += 1;
-                label1.Text = (lengthSnake - 2).ToString();
+                Count.Text = (lengthSnake - 2).ToString();
                 pointsSnake.Add(new Point(snake[lengthSnake - 1].Location.X, snake[lengthSnake - 1].Location.Y));
                 snake[lengthSnake].Location = pointsSnake[lengthSnake];
             }
