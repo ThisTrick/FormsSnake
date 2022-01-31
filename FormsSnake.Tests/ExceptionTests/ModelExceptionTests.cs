840683939813573a9ea134d34f840f8829061757
ThisTrickd.storozhyk <way.trickster@gmail.com>
2022-01-31 16:34:17 +0200

    Added ModelException

---
+++ FormsSnake.Tests/ExceptionTests/ModelExceptionTests.cs
@@ -0,0 +1,36 @@
+﻿using FormsSnake.Exception;
+using Xunit;
+
+namespace FormsSnake.Tests.ExceptionTests
+{
+    public class ModelExceptionTests
+    {
+        [Fact]
+        public void CtorWithModelName()
+        {
+            //Arrange and Act
+            ModelException modelException = new ModelException("TestModel");
+            //Assert
+            Assert.Equal("TestModel Exception.", modelException.Message);
+        }
+
+        [Fact]
+        public void CtorWithModelNameAndMessage()
+        {
+            //Arrange and Act
+            ModelException modelException = new ModelException("TestModel", "Test message");
+            //Assert
+            Assert.Equal("TestModel Exception: Test message.", modelException.Message);
+        }
+
+        [Fact]
+        public void CtorWithModelNameAndMessageAndInerException()
+        {
+            //Arrange and Act
+            ModelException modelException = new ModelException("TestModel", "Test message", new System.Exception());
+            //Assert
+            Assert.Equal("TestModel Exception: Test message.", modelException.Message);
+        }
+
+    }
+}
---
+++ FormsSnake/Exception/ModelException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsSnake.Exception
+{
+    [Serializable]
+    public class ModelException : System.Exception
+    {
+        public ModelException(string modelName) : base($"{modelName} Exception.") { }
+        public ModelException(string modelName, string message) : base($"{modelName} Exception: {message}.") { }
+        public ModelException(string modelName, string message, System.Exception inner) : base($"{modelName} Exception: {message}.", inner) { }
+        protected ModelException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    }
+}
