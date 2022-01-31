4fb396c4ee56ac621af15cab3a5e2921c7623143
ThisTrickd.storozhyk <way.trickster@gmail.com>
2022-01-31 16:56:12 +0200

    Implemented SnakeModel

--- FormsSnake/Model/SnakeModel.cs
+++ FormsSnake/Model/SnakeModel.cs
@@ -1,32 +1,52 @@
-﻿using System;
+﻿using FormsSnake.Exception;
 
 namespace FormsSnake.Model
 {
     public class SnakeModel : ISnakeModel
     {
+        private int Length { get; set; }
+
+        private bool IsAlive { get; set; }
+
+        public SnakeModel()
+        {
+            IsAlive = true;
+            Length = 3;
+        }
+
         public void Die()
         {
-            throw new NotImplementedException();
+            if (!IsAlive)
+                throw new ModelException(nameof(SnakeModel), "What is dead may never die");
+
+            IsAlive = false;
+            Length = 0;
         }
 
         public void EatAnApple()
         {
-            throw new NotImplementedException();
+            Length++;
         }
 
         public int GetLength()
         {
-            throw new NotImplementedException();
+            if (!IsAlive)
+                throw new ModelException(nameof(SnakeModel), "What is dead has no length");
+
+            return Length;
         }
 
         public bool HasAlive()
         {
-            throw new NotImplementedException();
+            return IsAlive;
         }
 
         public void Reborn()
         {
-            throw new NotImplementedException();
+            if (IsAlive)
+                throw new ModelException(nameof(SnakeModel), "The living does not reborn");
+            IsAlive = true;
+            Length = 3;
         }
     }
 }
