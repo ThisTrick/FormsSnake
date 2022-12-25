5280887db12c0dfa71cfe3fa12606a498e5bcf62
d.storozhyk <d.storozhyk@temabit.com>
2022-12-25 16:25:04 +0200

    Переназвав проект та рішення.

--- SnakeGame/Properties/AssemblyInfo.cs
+++ SnakeGame/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("FormsSnake")]
+[assembly: AssemblyTitle("SnakeGame")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FormsSnake")]
+[assembly: AssemblyProduct("SnakeGame")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
